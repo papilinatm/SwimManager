b11a5b476ca772edff3691a2b7ca7abcaf8ce07a
p_tm <papilina.tm@gmail.com>
2024-02-17 17:51:32 +0300

    net  8

--- SwimManager/Parser.cs
+++ SwimManager/Parser.cs
@@ -1,4 +1,8 @@
 ﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace SwimManager
--- SwimManager/Program.cs
+++ SwimManager/Program.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.CodeDom.Compiler;
+using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
+using System.Linq;
 using System.Text;
 using System.Xml.Linq;
 
